509c9b51c0fd0f5beb2b8e2808fa6c1a3f566899
Marie Locard-Paulet <m.locardpaulet@gmail.com>
2018-10-26 10:48:36 +0200

    Add help

--- app.R
+++ app.R
@@ -60,9 +60,15 @@ matriceColours2 <- data.frame("colorcodes" = colorcodes, "value" = colrep)
 ## UI:
 ############################################################################
 ui <- fluidPage(
-  
-  # Application title
-  titlePanel("LymphoAtlas"),
+  fluidRow(
+    column(3, titlePanel("LymphoAtlas")
+    ),
+    column(1, actionButton(inputId='ab1', label="?", 
+                           onclick ="window.open('https://masstools.ipbs.fr/lymphoAtlasHelp.html', '_blank')",
+                           style="color: #fff; background-color: #673a49; border-color: #000000")
+    )
+  ),
+  tags$style(type='text/css', "#ab1 { width:80%; margin-top: 25px; font-family : Cursive; font-weight: 900; font-size: 160%;}"),
   
   # Sidebar with a slider input for the phosphorylation site of interest: 
   
